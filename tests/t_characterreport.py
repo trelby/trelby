4f04dd96c5980d3df24d97887a08173b733c68c5
Osku Salerma <osku@iki.fi>
2005-07-25 10:45:50 +0000

    Add tests for location and character reports.

---
+++ tests/t_characterreport.py
@@ -0,0 +1,14 @@
+import characterreport
+import u
+
+# tests character report (just that it runs without exceptions, for now)
+
+def testBasic():
+    sp = u.load()
+    report = characterreport.CharacterReport(sp)
+    data = report.generate()
+
+    # try to catch cases where generate returns something other than a PDF
+    # document
+    assert len(data) > 200
+    assert data[:8] == "%PDF-1.4"
---
+++ tests/t_locationreport.py
@@ -0,0 +1,15 @@
+import locationreport
+import scenereport
+import u
+
+# tests location report (just that it runs without exceptions, for now)
+
+def testBasic():
+    sp = u.load()
+    report = locationreport.LocationReport(scenereport.SceneReport(sp))
+    data = report.generate()
+
+    # try to catch cases where generate returns something other than a PDF
+    # document
+    assert len(data) > 200
+    assert data[:8] == "%PDF-1.4"
