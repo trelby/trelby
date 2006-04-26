3abd5c96797f905323a3dfa90613f89cc79bf3dd
Osku Salerma <osku@iki.fi>
2006-04-26 19:39:09 +0000

    Update Makefile and build scripts to handle multiple Linux distributions.

--- Makefile
+++ Makefile
@@ -1,19 +1,22 @@
 PROG := blyte
 
-.PHONY = dist clean
+.PHONY = dist clean dists_prep
 
 $(PROG): linux.c Makefile
 	@echo Compiling...
 	@gcc -Wall -ansi -pedantic -D_GNU_SOURCE linux.c -O2 -o $(PROG) -lz
 	@strip $(PROG)
 
-prep: $(PROG)
+prep:
 	cd tools && ./make_names.py
 	make -C doc pdf && mv doc/manual.pdf .
 	gzip -c dict_en.dat > dict_en.dat.gz
 
 dist: $(PROG)
 	./gen_linux_dist.sh
+
+dists_prep:
+	./prep_linux_dists.sh
 
 clean:
 	rm -f $(PROG)
--- gen_linux_dist.sh
+++ gen_linux_dist.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
 
 set -eu
+
+# return true if the given file exists and its contents match the second
+# parameter, which is a regexp
+file_contains() {
+  if test -f $1 && grep -Eq "$2" $1; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# set $DIST_NAME to the linux distribution name/version the script is run
+# on
+set_dist_name() {
+  if file_contains "/etc/debian_version" "^3.1$"; then
+    DIST_NAME="debian-3.1"
+  elif file_contains "/etc/issue" "^Ubuntu 5\.10 "; then
+    DIST_NAME="ubuntu-5.10"
+  elif file_contains "/etc/fedora-release" "^Fedora Core release 5 "; then
+    DIST_NAME="fedora-5"
+  elif file_contains "/etc/issue" " SUSE LINUX 10\.0 "; then
+    DIST_NAME="suse-10.0"
+  else
+    echo "Error: Unknown distribution!"
+    exit 1
+  fi
+}
+
+set_dist_name
+
 VER=$(grep 'misc.version =' blyte.py | cut -b24- | perl -pe 's/"//g;')
 DIR="linux-dist/blyte-$VER"
 
@@ -14,6 +44,15 @@ cp Makefile.install $DIR/Makefile
 cd linux-dist
 tar cvf "blyte-$VER.tar" "blyte-$VER"
 gzip -9 "blyte-$VER.tar"
-mv "blyte-$VER.tar.gz" ..
+
+fname="blyte-$VER-$DIST_NAME.tar.gz"
+mv "blyte-$VER.tar.gz" "../$fname"
+
 cd ..
 rm -rf linux-dist
+
+if ! test $DIST_NAME = "debian-3.1"; then
+  cp $fname /mnt/debian-3.1/home/osku
+fi
+
+echo "Status: '$fname' created"
