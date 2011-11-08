ed54b24d7df598917173ed7336a91b1d36ec729a
Anil Gulecha <anil.verve@gmail.com>
2011-11-08 21:09:09 +0000

    Change splash screen.

--- splash.py
+++ splash.py
@@ -11,26 +11,18 @@ class SplashWindow(wx.Frame):
     def __init__(self, parent, delay):
         wx.Frame.__init__(
             self, parent, -1, "Splash",
-            style = wx.FRAME_FLOAT_ON_PARENT | wx.STAY_ON_TOP | wx.NO_BORDER)
+            style = wx.FRAME_FLOAT_ON_PARENT | wx.NO_BORDER)
 
         if not SplashWindow.inited:
             SplashWindow.inited = True
             wx.Image_AddHandler(wx.JPEGHandler())
-
-        fileName = u"logo.jpg"
-        fileData = util.loadFile(fileName, parent)
-
-        if not fileData:
-            self.abort()
         
-        self.pic = wx.Bitmap(fileName, wx.BITMAP_TYPE_JPEG)
-        if not self.pic.Ok():
-            self.abort()
+        self.pic = misc.getBitmap("resources/logo.jpg")
 
-        w, h = (self.pic.GetWidth(), self.pic.GetHeight())
-
-        if (w != 640) or (h != 440):
-            self.abort()
+        if self.pic.Ok():
+            w, h = (self.pic.GetWidth(), self.pic.GetHeight())
+        else:
+            w, h = (375, 300)
 
         util.setWH(self, w, h)
         self.CenterOnScreen()
@@ -38,22 +30,17 @@ class SplashWindow(wx.Frame):
         self.textColor = wx.Colour(0, 0, 0)
 
         self.font = util.createPixelFont(
-            17, wx.FONTFAMILY_MODERN, wx.NORMAL, wx.NORMAL)
+            14, wx.FONTFAMILY_MODERN, wx.NORMAL, wx.NORMAL)
 
         if delay != -1:
             self.timer = wx.Timer(self)
             wx.EVT_TIMER(self, -1, self.OnTimer)
             self.timer.Start(delay, True)
-        else:
-            wx.EVT_LEFT_DOWN(self, self.OnClick)
+
+        wx.EVT_LEFT_DOWN(self, self.OnClick)
 
         wx.EVT_PAINT(self, self.OnPaint)
         wx.EVT_CLOSE(self, self.OnCloseWindow)
-
-    def abort(self):
-        wx.MessageBox("Error opening splash screen.", "Error", wx.OK,
-                      self.GetParent())
-        sys.exit()
         
     def OnClick(self, event):
         self.Close()
@@ -64,10 +51,11 @@ class SplashWindow(wx.Frame):
         dc.SetFont(self.font)
         dc.SetTextForeground(self.textColor)
 
-        dc.DrawBitmap(self.pic, 0, 0, False)
+        if self.pic.Ok():
+            dc.DrawBitmap(self.pic, 0, 0, False)
 
-        util.drawText(dc, "Version %s" % misc.version,
-                      630, 346, util.ALIGN_RIGHT)
+        util.drawText(dc, "Version %s" % (misc.version),
+                      200, 170, util.ALIGN_RIGHT)
         
     def OnTimer(self, event):
         self.timer.Stop()
@@ -76,4 +64,3 @@ class SplashWindow(wx.Frame):
     def OnCloseWindow(self, event):
         self.Destroy()
         self.Refresh()
-        
