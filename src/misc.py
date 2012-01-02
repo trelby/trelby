9432dc3bc1522725f79c6daa866d6cc1df916c6e
Osku Salerma <osku.salerma@gmail.com>
2012-01-02 13:30:32 +0000

    Bump version number to 2.1-dev.

--- src/misc.py
+++ src/misc.py
@@ -17,7 +17,7 @@ def init(doWX = True):
     # prefix used for temp files
     tmpPrefix = "trelby-tmp-"
 
-    version = "2.0-dev"
+    version = "2.1-dev"
 
     isWindows = False
     isUnix = False
