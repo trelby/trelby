1e4ffd77e5089089760f71aa7411b4c07609d0b1
Anil Gulecha <anil.verve@gmail.com>
2012-08-25 12:24:54 +0530

    Change to a better looking tab close character.

--- src/misc.py
+++ src/misc.py
@@ -262,6 +262,7 @@ class MyTabCtrl(wx.Window):
 
         # initialized in OnPaint since we don't know our height yet
         self.font = None
+        self.boldfont = None
 
         self.SetMinSize(wx.Size(
                 self.paddingX * 2 + self.arrowWidth * 2 + self.arrowSpacing +\
@@ -461,12 +462,13 @@ class MyTabCtrl(wx.Window):
             textH = h - self.textY - 1
             self.font = util.createPixelFont(
                 textH, wx.FONTFAMILY_DEFAULT, wx.NORMAL, wx.NORMAL)
-
-        dc.SetFont(self.font)
+            self.boldfont = util.createPixelFont(
+                textH, wx.FONTFAMILY_DEFAULT, wx.NORMAL, wx.BOLD)
 
         maxTab = self.getLastVisibleTab()
 
         for i in range(self.firstTab, maxTab + 1):
+            dc.SetFont(self.font)
             p = self.pages[i]
 
             dc.DestroyClippingRegion()
@@ -491,7 +493,8 @@ class MyTabCtrl(wx.Window):
             dc.DrawText(p[1], xpos + self.paddingX, self.textY)
 
             dc.DestroyClippingRegion()
-            dc.DrawText("x", xpos + tabW - self.paddingX * 2, self.textY)
+            dc.SetFont(self.boldfont)
+            dc.DrawText(chr(215), xpos + tabW - self.paddingX * 2, self.textY)
 
             xpos += tabW
 
