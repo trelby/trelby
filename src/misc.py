ca3467aa0e9a23c33188ee02c17939909a832460
Osku Salerma <osku.salerma@gmail.com>
2014-06-30 10:06:19 +0100

    Use sys.platform instead of wx.Platform to determine the OS.

--- src/misc.py
+++ src/misc.py
@@ -23,10 +23,10 @@ def init(doWX = True):
     isWindows = False
     isUnix = False
 
-    if wx.Platform == "__WXMSW__":
-        isWindows = True
-    else:
+    if sys.platform.startswith("linux") or sys.platform.startswith("darwin"):
         isUnix = True
+    else:
+        isWindows = True
 
     # does this platform support using Python's unicode strings in various
     # filesystem calls; if not, we need to convert filenames to UTF-8
