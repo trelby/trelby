ed35638ef87ae1d598653df82a44cf7731763bb8
Gwyn Ciesla <gwync@protonmail.com>
2025-08-20 16:03:35 -0500

    Chocolatey packaging.

---
+++ choco/tools/chocolateyinstall.ps1
@@ -0,0 +1,2 @@
+﻿Start-ChocolateyProcessAsAdmin "pip3 install trelby==2.4.15"
+Install-ChocolateyShortcut -ShortcutFilePath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\trelby.lnk" -TargetPath "C:\Python313\Scripts\trelby.exe" -IconLocation "C:\Python313\DLLs\py.ico"
