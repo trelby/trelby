86079b162e6514754ce0a2f3ea323eaca81146e7
Gwyn Ciesla <gwync@protonmail.com>
2025-10-03 10:50:24 -0500

    Fix choco install version

--- choco/tools/chocolateyinstall.ps1
+++ choco/tools/chocolateyinstall.ps1
@@ -1,2 +1,2 @@
-﻿Start-ChocolateyProcessAsAdmin "pip3 install trelby==2.4.16"
+﻿Start-ChocolateyProcessAsAdmin "pip3 install trelby==2.4.16.2"
 Install-ChocolateyShortcut -ShortcutFilePath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\trelby.lnk" -TargetPath "C:\Python313\Scripts\trelby.exe" -IconLocation "C:\Python313\Lib\site-packages\trelby\resources\icon256.ico"
