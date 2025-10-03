dbbf85ff3550f34b391adf53cd1d23cb43db5878
Gwyn Ciesla <gwync@protonmail.com>
2025-10-03 10:07:41 -0500

    2.4.16

--- t_01_update_pot.sh
+++ t_01_update_pot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 xgettext -j -L Python -o po/trelby.pot \
 	--package-name=trelby \
-        --package-version=2.4.15 \
+        --package-version=2.4.16 \
         --copyright-holder="Gwyn Ciesla <gwync@protonmail.com>" \
 	$(find ./trelby -type f -name '*.py')
